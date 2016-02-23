306b31ff008eeaa590465f3335341e6f999a8589
gernest <geofreyernest@live.com>
2016-02-23 11:36:54 +0300

    Add test for Lex

--- lexer/combine_test.go
+++ lexer/combine_test.go
@@ -1,1 +1,18 @@
 package lexer
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+)
+
+func TestLex(t *testing.T) {
+	src, err := ioutil.ReadFile("fixture/modem.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = Lex(bytes.NewReader(src))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
