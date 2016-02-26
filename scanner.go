5c3cf312d29912e4661c05dad32b0aa8c73db5d1
gernest <geofreyernest@live.com>
2016-02-26 12:42:11 +0300

    First draft of scanner

---
+++ scanner.go
@@ -0,0 +1,147 @@
+package config
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"io"
+	"unicode"
+)
+
+type TokenType int
+
+const (
+	EOF TokenType = iota
+	Comment
+	Section
+	WHiteSpace
+	Ident
+	Operand
+	OpenBrace
+	ClosingBrace
+)
+
+type Token struct {
+	Type   TokenType
+	Text   string
+	Line   int
+	Column int
+}
+
+type Scanner struct {
+	r      *bufio.Reader
+	txt    *bytes.Buffer
+	tok    Token // the last token
+	line   int
+	err    error
+	column int
+}
+
+func NewScanner(src io.Reader) *Scanner {
+	return &Scanner{
+		r:   bufio.NewReader(src),
+		txt: &bytes.Buffer{},
+	}
+}
+
+func (s *Scanner) Scan() (*Token, error) {
+	ch, _, err := s.r.ReadRune()
+	if err != nil {
+		return nil, err
+	}
+	if isIdent(ch) {
+		tok := &Token{}
+		tok.Type = Ident
+		tok.Text = string(ch)
+		return tok, nil
+	}
+	s.r.UnreadRune()
+	switch ch {
+	case ';':
+		return s.scanComment()
+	case ' ', '\t', '\n', '\r':
+		return s.scnWHiteSpace()
+	case '=':
+		s.r.ReadRune()
+		tok := &Token{}
+		tok.Type = Operand
+		tok.Text = string(ch)
+		return tok, nil
+	case '[':
+		s.r.ReadRune()
+		tok := &Token{}
+		tok.Type = OpenBrace
+		tok.Text = string(ch)
+		return tok, nil
+	case ']':
+		s.r.ReadRune()
+		tok := &Token{}
+		tok.Type = ClosingBrace
+		tok.Text = string(ch)
+		return tok, nil
+	}
+	return nil, errors.New("unrecognized token " + string(ch))
+}
+
+func (s *Scanner) scanComment() (*Token, error) {
+	tok := &Token{}
+	buf := &bytes.Buffer{}
+END:
+	for {
+		ch, size, err := s.r.ReadRune()
+		if err != nil {
+			if err.Error() == io.EOF.Error() {
+				break END
+			}
+			return nil, err
+		}
+		switch ch {
+		case '\n', '\r':
+			s.r.UnreadRune()
+			break END
+		default:
+			buf.WriteRune(ch)
+			s.column = s.column + size
+		}
+	}
+	tok.Column = s.column
+	tok.Type = Comment
+	tok.Text = buf.String()
+	tok.Line = s.line
+	return tok, nil
+}
+func (s *Scanner) scnWHiteSpace() (*Token, error) {
+	tok := &Token{}
+	buf := &bytes.Buffer{}
+END:
+	for {
+		ch, size, err := s.r.ReadRune()
+		if err != nil {
+			if err.Error() == io.EOF.Error() {
+				break END
+			}
+			return nil, err
+		}
+		switch ch {
+		case '\n', '\r':
+			buf.WriteRune(ch)
+			s.line++
+			s.column = 0
+		case ' ', '\t':
+			buf.WriteRune(ch)
+			s.column = s.column + size
+		default:
+			s.r.UnreadRune()
+			break END
+		}
+	}
+	tok.Column = s.column
+	tok.Type = Comment
+	tok.Text = buf.String()
+	tok.Line = s.line
+	return tok, nil
+}
+
+func isIdent(ch rune) bool {
+	return ch == '_' || ch == '-' || ch == '+' || unicode.IsLetter(ch) || unicode.IsDigit(ch)
+}
---
+++ scanner_test.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"testing"
+)
+
+func TestScanner(t *testing.T) {
+	src, err := ioutil.ReadFile("fixture/modem.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	s := NewScanner(bytes.NewReader(src))
+	var tok *Token
+	for err == nil {
+		tok, err = s.Scan()
+		if err != nil {
+			if err.Error() != io.EOF.Error() {
+				t.Fatal(err)
+			}
+		}
+		if tok != nil {
+			fmt.Println(tok.Text)
+		}
+	}
+}
