fc30f317bb4c2a409fa577e6a77441f4d5117d47
gernest <geofreyernest@live.com>
2016-03-07 13:17:18 +0300

    Add a dummy main function

--- cmd/fconf/main.go
+++ cmd/fconf/main.go
@@ -1,10 +1,17 @@
 package main
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 type DeviceConfig interface {
 	Name() string
 	LoadJSON(io.Reader) error
 	Save() error
 	ToJSON(io.Writer) error
+}
+
+func main() {
+	fmt.Println("hello box")
 }
