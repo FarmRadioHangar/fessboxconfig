0a4dd686b64447af0ef99709ed0e64b96f8f35a2
gernest <geofreyernest@live.com>
2016-03-07 16:43:04 +0300

    Start the base for webserver

--- cmd/fconf/main.go
+++ cmd/fconf/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
+	"log"
+	"net/http"
+
+	"github.com/gernest/alien"
 )
 
 type DeviceConfig interface {
@@ -13,5 +18,26 @@ type DeviceConfig interface {
 }
 
 func main() {
-	fmt.Println("hello box")
+	port := flag.Int64("port", 8970, "Specify the port in which to run the magager")
+	flag.Parse()
+	s := alien.New()
+	w := newWeb()
+	s.Get("/", w.Home)
+	s.Get("/device/dongle", w.Dongle)
+	log.Printf(" starting server on  localhost:%d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), s))
+
+}
+
+type web struct {
+}
+
+func newWeb() *web {
+	return &web{}
+}
+
+func (ww *web) Home(w http.ResponseWriter, r *http.Request) {
+}
+
+func (ww *web) Dongle(w http.ResponseWriter, r *http.Request) {
 }
