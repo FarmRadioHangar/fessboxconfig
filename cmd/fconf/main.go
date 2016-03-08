9b7946a3a6d47e4d0a4f282997d9e4d52543cf66
gernest <geofreyernest@live.com>
2016-03-08 15:20:40 +0300

    Cleanup the base for backend
    This add support for static assets, and sets up a boilerplate for frontend
    development

--- cmd/fconf/main.go
+++ cmd/fconf/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"html/template"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -15,26 +18,70 @@ type DeviceConfig interface {
 	ToJSON(io.Writer) error
 }
 
+type Config struct {
+	Port           int64  `json:"port"`
+	Host           string `json:"host"`
+	StaticDir      string `json:"static_dir"`
+	IndexFile      string `json:"index_file"`
+	AsteriskConfig string `json:"asterisk_config_dir"`
+}
+
+func defaultConfig() *Config {
+	return &Config{
+		Port:           8080,
+		Host:           "",
+		StaticDir:      "static",
+		IndexFile:      "index.html",
+		AsteriskConfig: "/etc/asterisk",
+	}
+}
+
 func main() {
-	port := flag.Int64("port", 8970, "Specify the port in which to run the magager")
+	c := flag.String("c", "conf/config.json", "path to the configuration file")
 	flag.Parse()
+	b, err := ioutil.ReadFile(*c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cfg := &Config{}
+	err = json.Unmarshal(b, cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
 	s := http.NewServeMux()
-	w := newWeb()
+	w := newWeb(cfg)
 	s.HandleFunc("/", w.Home)
 	s.HandleFunc("/device/dongle", w.Dongle)
-	log.Printf(" starting server on  localhost:%d\n", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), s))
-
+	s.Handle("/static/",
+		http.StripPrefix("/static/", http.FileServer(http.Dir(cfg.StaticDir))))
+	log.Printf(" starting server on  localhost:%d\n", cfg.Port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), s))
 }
 
 type web struct {
+	cfg     *Config
+	homeTpl *template.Template
 }
 
-func newWeb() *web {
-	return &web{}
+func newWeb(cfg *Config) *web {
+	w := &web{cfg: cfg}
+	b, err := ioutil.ReadFile(cfg.IndexFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tpl, err := template.New("index").Parse(string(b))
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.homeTpl = tpl
+	return w
 }
 
 func (ww *web) Home(w http.ResponseWriter, r *http.Request) {
+	err := ww.homeTpl.ExecuteTemplate(w, "index", nil)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 func (ww *web) Dongle(w http.ResponseWriter, r *http.Request) {
