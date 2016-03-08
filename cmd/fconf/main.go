852df0e58c01a814c64f81ff97afcb090689af19
gernest <geofreyernest@live.com>
2016-03-08 10:06:21 +0300

    Remove external dependency

--- cmd/fconf/main.go
+++ cmd/fconf/main.go
@@ -6,8 +6,6 @@ import (
 	"io"
 	"log"
 	"net/http"
-
-	"github.com/gernest/alien"
 )
 
 type DeviceConfig interface {
@@ -20,10 +18,10 @@ type DeviceConfig interface {
 func main() {
 	port := flag.Int64("port", 8970, "Specify the port in which to run the magager")
 	flag.Parse()
-	s := alien.New()
+	s := http.NewServeMux()
 	w := newWeb()
-	s.Get("/", w.Home)
-	s.Get("/device/dongle", w.Dongle)
+	s.HandleFunc("/", w.Home)
+	s.HandleFunc("/device/dongle", w.Dongle)
 	log.Printf(" starting server on  localhost:%d\n", *port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), s))
 
