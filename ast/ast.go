3e4ecc547c8946b0990277079321c9042c99533b
gernest <geofreyernest@live.com>
2016-03-17 11:32:21 +0300

    Add definitions for ast

--- ast/ast.go
+++ ast/ast.go
@@ -1,1 +1,18 @@
 package ast
+
+type Node interface {
+	Begin() int
+	End() int
+	Text() string
+}
+
+type Context struct {
+	Head []Node
+	Body []Node
+}
+
+type AsignStmt struct {
+	Left  []Node
+	Equal Node
+	Right []Node
+}
