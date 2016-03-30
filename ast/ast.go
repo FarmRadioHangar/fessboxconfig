5330f0b50533c0dd2a91610ff75726c8049412e1
gernest <geofreyernest@live.com>
2016-03-30 10:09:29 +0300

    Add documentation

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,14 @@
 package ast
 
+//Node is an interface represent a meaningful chunch of text in an input stream
 type Node interface {
 	Begin() int
 	End() int
 	Text() string
 }
 
+//Context is a scetion in asterisk configuration file. It holds all the
+//information of the section.
 type Context struct {
 	Head        Node
 	Templates   []Node
@@ -14,16 +17,22 @@ type Context struct {
 	Comments    []Node
 }
 
+//Template is a Context but that is used as a template to composing other
+//contexts by the asterisk parser.
 type Template Context
 
+//AsignStmt holds information about assignmant definitions in the configuration
+//file.
 type AsignStmt struct {
 	Left  []Node
 	Equal Node // =
 	Right []Node
 }
 
+//Object is a special kind of assignment statement
 type Object AsignStmt
 
+//File is the abstract representation of the configuration file
 type File struct {
 	Comments    []Node
 	Contexts    []Context
