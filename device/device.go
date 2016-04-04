81ab8dcc61923ccf67b1832a85998bbd80d2dc1c
gernest <geofreyernest@live.com>
2016-04-04 12:51:25 +0300

    Add *Conn.Open
    This open serial port for the device represented by Conn

--- device/device.go
+++ device/device.go
@@ -5,3 +5,19 @@ import "github.com/tarm/serial"
 type Manager struct {
 	devices map[string]serial.Config
 }
+
+type Conn struct {
+	device serial.Config
+	port   *serial.Port
+	isOpen bool
+}
+
+func (c *Conn) Open() error {
+	p, err := serial.OpenPort(&c.device)
+	if err != nil {
+		return nil
+	}
+	c.port = p
+	c.isOpen = true
+	return nil
+}
