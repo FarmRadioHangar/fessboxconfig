669f5e4411903529c186c3ace6964cdabbecfc49
gernest <geofreyernest@live.com>
2016-07-12 11:24:22 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-BIN_DIR=/usr/bin
 SCRIPT_DIR=$PWD/scripts
 
 
@@ -16,11 +15,6 @@ id fconf &>/dev/null
 if [[ $? -ne 0 ]]; then
     useradd -r -K USERGROUPS_ENAB=yes -M fconf -s /bin/false -d /etc/fconf
 fi
-
-test -d $LOG_DIR || mkdir -p $LOG_DIR
-chown -R -L fconf:fconf $LOG_DIR
-chmod 755 $LOG_DIR
-
 
 
 if [[ -f /etc/os-release ]]; then
